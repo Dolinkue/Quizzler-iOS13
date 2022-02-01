53529ef2110fba986765e4112dacd7865ee24ca6
dolinkue <dolinkue_n@me.com>
2022-02-01 17:10:34 -0300

    mejorando botones y preguntas

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var trueBottom: UIButton!
+    
+    @IBOutlet weak var falseBottom: UIButton!
+    
+    let quiz = [
+        ["four + two is equal to six","True"],
+        ["five - three is greater than one","True"],
+        ["three + eigth is less tahn ten","false"]
+    ]
+     var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        updateUI()
+        
     }
 
-
+    @IBAction func answerbotton(_ sender: UIButton) {
+        
+        let userAnwers = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnwers == actualAnswer{
+            print("Right")
+        }else{
+            print("wrong")
+        }
+        
+        if questionNumber + 1 < quiz.count {
+            
+            questionNumber += 1
+            
+            }
+        else {
+            questionNumber = 0
+        }
+        updateUI()
+    }
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
+      
+        
+    }
 }
 
